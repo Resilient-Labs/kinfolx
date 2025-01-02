644ea6949f5ecd7a22b7f81f4ccabb92b7e42250
trialnerr <54381853+trialnerr@users.noreply.github.com>
2025-01-02 16:44:46 -0500

    Add sign in and sign up pages from clerk. Configure react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import SignInPage from './components/signInPage/signInPage.jsx';
+import SignUpPage from './components/signUpPage/signUpPage.jsx';
+
+//this is related to clerk
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
+//this is related to frontend routing
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      { path: '/sign-in/*', element: <SignInPage /> },
+      { path: '/sign-up/*', element: <SignUpPage /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>
+);
