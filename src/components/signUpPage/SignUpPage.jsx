3327e9f44abdf3f20725f04c398c7596bc74884b
trialnerr <54381853+trialnerr@users.noreply.github.com>
2025-01-02 18:46:32 -0500

    Formating

--- src/components/signUpPage/SignUpPage.jsx
+++ src/components/signUpPage/SignUpPage.jsx
@@ -1,10 +1,10 @@
-import { SignUp } from "@clerk/clerk-react";
-import "./signUpPage.css";
+import { SignUp } from '@clerk/clerk-react';
+import './signUpPage.css';
 
 function SignUpPage() {
   return (
-    <div className="signUpPage">
-      <SignUp path="/sign-up" signInUrl="sign-in" />
+    <div className='signUpPage'>
+      <SignUp path='/sign-up' signInUrl='sign-in' />
     </div>
   );
 }
