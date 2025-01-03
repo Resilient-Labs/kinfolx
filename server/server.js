dd96d1ed3e224beb2688f297c2bf72f25622c0d4
trialnerr <54381853+trialnerr@users.noreply.github.com>
2025-01-03 12:13:40 -0500

    Changed server code to use requireAuth middleware from clerk/express

--- server/server.js
+++ server/server.js
@@ -1,34 +1,28 @@
 import express from 'express'
 import logger from 'morgan'
 import { config } from 'dotenv'
-import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node'
+import { requireAuth } from '@clerk/express'
 
 config({ path: './config/.env' })
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(requireAuth())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(logger('dev'))
 
-app.get('/api/test', ClerkExpressRequireAuth(), async (req, res) => {
-    return res.json({ itWorks: 'true' })
-})
+// EXAMPLES FOR HOW TO USE CLERK MIDDLEWARE
+// Use a custom sign-in URL instead of the environment variable
+app.get(
+    '/api/protected',
+    requireAuth({ signInUrl: '/sign-in' }),
+    (req, res) => {
+        res.json({ response: 'This is a protected route' })
+    },
+)
 
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`)
 })
-
-// Get user's chats (Example usage)
-// app.get("/api/userchats", ClerkExpressRequireAuth(), async (req, res) => {
-//     const { userId } = req.auth; // Get userId from Clerk authentication
-
-//     try {
-//         const userChats = await UserChats.findOne({ userId });
-//         res.status(200).send(userChats ? userChats.chats : []);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).send("Error fetching user chats!");
-//     }
-// });
