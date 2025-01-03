f93f098dd13c2ae8a6f498520c9a3a47afa85f45
trialnerr <54381853+trialnerr@users.noreply.github.com>
2025-01-03 11:20:46 -0500

    Prettier formating changes.

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,19 +1,19 @@
-import { useAuth } from '@clerk/clerk-react';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { useAuth } from '@clerk/clerk-react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router'
 
 const ProtectedRoute = ({ children }) => {
-  const { userId, isLoaded } = useAuth();
-  const navigate = useNavigate();
+    const { userId, isLoaded } = useAuth()
+    const navigate = useNavigate()
 
-  useEffect(() => {
-    if (isLoaded && !userId) {
-      navigate('/sign-in');
-    }
-  }, [isLoaded, userId, navigate]);
+    useEffect(() => {
+        if (isLoaded && !userId) {
+            navigate('/sign-in')
+        }
+    }, [isLoaded, userId, navigate])
 
-  if (!isLoaded) return <div>Loading...</div>;
+    if (!isLoaded) return <div>Loading...</div>
 
-  return { children };
-};
-export default ProtectedRoute;
+    return { children }
+}
+export default ProtectedRoute
