6dc33d7007dfb9ae7c280f8840a7f0493d2adf3e
trialnerr <54381853+trialnerr@users.noreply.github.com>
2025-01-03 11:07:43 -0500

    Formating change. Add semicolon

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    //a proxy that allows you to send fetch requests on the frontend to example '/api/reviews'
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
+    server: {
+        //a proxy that allows you to send fetch requests on the frontend to example '/api/reviews'
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000',
+                changeOrigin: true,
+            },
+        },
     },
-  },
-  plugins: [react()],
-});
+    plugins: [react()],
+})
