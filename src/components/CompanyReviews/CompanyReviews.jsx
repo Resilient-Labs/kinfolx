ee2bde9356aa60198ee324bb206b723b04755056
gkelly10 <gabriellekelly10101@gmail.com>
2025-01-04 17:45:21 -0500

    Added static company reviews component and css

---
+++ src/components/CompanyReviews/CompanyReviews.jsx
@@ -0,0 +1,11 @@
+import './companyReviews.css'
+
+const CompanyReviews = () => {
+    return (
+        <div id="reviews" className="content-box">
+            Summary of Reviews
+        </div>
+    )
+}
+
+export default CompanyReviews
