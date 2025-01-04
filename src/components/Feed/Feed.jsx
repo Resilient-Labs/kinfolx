cd9b37357ddebff656a30d5ca9cbf427873f0619
intercalaris <ymaviv@gmail.com>
2025-01-03 19:14:37 -0500

    Added feed component and corresponding styles

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './feed.css'
+
+export default function Feed() {
+    return (
+        <section className="feed">
+            {/* update to dynamically populate */}
+            <div className="feed-item">Spotify is a great company</div>
+            <div className="feed-item">Costco defends DEI</div>
+            <div className="feed-item">Yuhh</div>
+            <div className="feed-item">Sample content</div>
+            <div className="feed-item">Sampul content</div>
+            <div className="feed-item">Sampil content</div>
+            <div className="feed-item">Sampill content</div>
+        </section>
+    )
+}
