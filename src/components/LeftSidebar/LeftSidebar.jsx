625413557d66cc2bfdf684156a494b153d913b08
intercalaris <ymaviv@gmail.com>
2025-01-03 19:15:20 -0500

    Added left sidebar component and corresponding styles

---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './leftSidebar.css'
+
+export default function LeftSidebar() {
+    return (
+        <aside className="left-sidebar">
+            <button className="action-btn add-review">Add Review</button>
+            <button className="action-btn create-post">Create Post</button>
+            <section className="favorite-companies">
+                <h3>Favorite Companies</h3>
+                <ul className="favorite-list">
+                    {/* update to dynamically populate */}
+                    <li>Favorite company 1</li>
+                    <li>Favorite company 2</li>
+                    <li>Favorite company 3</li>
+                    <li>Favorite company 4</li>
+                </ul>
+            </section>
+        </aside>
+    )
+}
