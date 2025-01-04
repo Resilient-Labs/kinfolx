415b0b800846950a1bc741814cd69f18890dc375
gkelly10 <gabriellekelly10101@gmail.com>
2025-01-04 17:45:02 -0500

    Added static company overview component and css

---
+++ src/components/CompanyOverview/CompanyOverview.jsx
@@ -0,0 +1,11 @@
+import './companyOverview.css'
+
+const CompanyOverview = () => {
+    return (
+        <div id="overview" className="content-box">
+            Overview of company
+        </div>
+    )
+}
+
+export default CompanyOverview
