de4285b5444d02ddac6dfe31f20fd0ccf6ef8ce5
gkelly10 <gabriellekelly10101@gmail.com>
2025-01-04 17:44:41 -0500

    Added static company banner component and css

---
+++ src/components/CompanyBanner/CompanyBanner.jsx
@@ -0,0 +1,23 @@
+import './companyBanner.css'
+
+ const CompanyBanner = () => {
+    return (
+            <section className="company-info">
+                <div className="company-logo">Company Logo</div>
+                <nav className="company-tabs">
+                    <a href="#overview" className="tab">
+                        Overview
+                    </a>
+                    <a href="#reviews" className="tab">
+                        Reviews
+                    </a>
+                    <a href="#salaries" className="tab">
+                        Salaries
+                    </a>
+                </nav>
+                <button className="btn action-btn">Review This Company</button>
+            </section>
+    )
+}
+
+export default CompanyBanner
