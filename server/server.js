c6a7b97333925bfcd5a3c8edc66d17c41547cdb9
Franceska <franceska@fullstackfrench.com>
2025-01-05 13:36:20 -0500

    #8 addressed server.js conflicts

--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
+import express from 'express';
+import logger from 'morgan';
+import { config } from 'dotenv';
+import { requireAuth } from '@clerk/express'
+
+const companyRouter = require("./routes/company");
+const reviewRouter = require("./routes/reviews");
+const userRouter = require("./routes/user");
+config({ path: './config/.env' });
+
+//to check if our env connection is correct
+console.log(process.env.DB_STRING);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
+
+app.get('/api/test', ClerkExpressRequireAuth(), async (req, res) => {
+  return res.json({ 'itWorks': 'true' });
+});
 import express from 'express'
 import logger from 'morgan'
 import { config } from 'dotenv'
@@ -26,3 +49,7 @@ app.get(
 app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`)
 })
+
+app.use('/api/company', companyRouter) 
+app.use('/api/review', reviewRouter)
+app.use('api/user', userRouter) 
