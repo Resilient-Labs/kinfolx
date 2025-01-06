b7dbf8942bae14e548aa68cc9d79536f2e635761
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-01-06 12:22:02 -0500

    Delete server/models/Profile.js
    -deleted Profile Model

--- server/models/Profile.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-const ProfileSchema = new Schema({
-    //I have not created a schema for profile because Clerk handles everything profile related.
-})
-
-export default mongoose.model('Profile', ProfileSchema);
