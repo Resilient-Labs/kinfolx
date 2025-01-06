0c5f726651e4a968a3e6d36e282ca61d5bc063f8
intercalaris <ymaviv@gmail.com>
2025-01-06 16:36:35 -0500

    #22 Added company component and subcomponents and respective CSS modules

---
+++ src/components/CompanyReviewSummary/CompanyReviewSummary.jsx
@@ -0,0 +1,9 @@
+import './companyReviewSummary.css'
+
+const CompanyReviewSummary = () => {
+  return (
+    <div id="reviews" className="content-box">Summary of Reviews</div>
+  )
+}
+
+export default CompanyReviewSummary
---
+++ src/components/CompanySalaries/CompanySalaries.jsx
@@ -0,0 +1,11 @@
+import './companySalaries.css'
+
+const CompanySalaries = () => {
+    return (
+        <div id="salaries" className="content-box">
+            Summary of Salaries
+        </div>
+    )
+}
+
+export default CompanySalaries
