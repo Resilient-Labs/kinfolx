b7585e76a7108188ce0bfb0197ccc3c540ad2467
Rosy Basil-Kalu <basilkalurosy@gmail.com>
2025-01-07 18:25:04 -0500

    updated edit review controller #2

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -4,37 +4,7 @@ import Company from '../models/Company.js'
 //possible controller for reviews
 
 const reviewController = {
-    createReview: async (req, res) => {
-        try {
-            const { ratings, comment } = req.body
-            const { companyId } = req.params
-
-            // Convert ratings to numbers
-            const numericRatings = {}
-            for (const [key, value] of Object.entries(ratings)) {
-                numericRatings[key] = Number(value)
-            }
-
-            // Fetch company details
-            const company = await Company.findById(companyId)
-            if (!company) {
-                return res.status(400).send('Company name is required')
-            }
-            await Review.create({
-                companyId,
-                companyName: company.name,
-                ratings: numericRatings,
-                comment,
-                user: req.user.id,
-            })
-            console.log('Review has been added!')
-            res.redirect('/reviews')
-            console.log(Review)
-        } catch (err) {
-            console.log(err)
-            res.status(500).send('Error creating review')
-        }
-    },
+    
     getUserReview: async (req, res) => {
         try {
             const userId = req.params.userId
