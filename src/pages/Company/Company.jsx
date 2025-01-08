cc4e037e1a5e577b12662f92681dd8a4fcb87f3b
AOA19 <aakinyemi19@gmail.com>
2025-01-07 22:32:54 -0500

    #38 - Removed the hard coded companies list and added a fetch request to dynamically render all companies from server. When companies clicked should now navigate to specific company based on ID. - Made one small change to company controller getCompany function
    Co-authored-by: Angel Morris <angel.morris920@gmail.com>

--- src/pages/Company/Company.jsx
+++ src/pages/Company/Company.jsx
@@ -1,10 +1,37 @@
+import { useParams } from 'react-router'
 import './company.css'
 import CompanyBanner from '../../components/CompanyBanner/CompanyBanner.jsx'
 import CompanyOverview from '../../components/CompanyOverview/CompanyOverview.jsx'
 import CompanyReviewSummary from '../../components/CompanyReviewSummary/CompanyReviewSummary.jsx'
 import CompanySalaries from '../../components/CompanySalaries/CompanySalaries.jsx'
+import { useState, useEffect } from 'react'
 
 const Company = () => {
+    const [company, setCompany] = useState([])
+    let params = useParams()
+    console.log({params})
+
+       useEffect(() => {
+            const fetchCompany = async () => {
+                try {
+                    const response = await fetch(`/api/company/${params.id}`);
+                    console.log({response});
+                    if (!response.ok) throw new Error("Failed to fetch company.");
+        
+                    const data = await response.json();
+                    console.log(data)
+                    setCompany(data); 
+                    
+                    
+                } catch (error) {
+                    console.error("Error fetching companies:", error);
+                }
+            };
+        
+            fetchCompany();
+        }, [params.id]);
+
+        console.log(company)
     return (
         <main className="company-container">
             <CompanyBanner />
