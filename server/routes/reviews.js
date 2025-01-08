5c65075cce78663ebd5f6c939d381c05bc0d6af9
Rosy Basil-Kalu <basilkalurosy@gmail.com>
2025-01-08 17:46:04 -0500

    updated editReview controller

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,6 +1,4 @@
 import express from 'express'
-<<<<<<< HEAD
-<<<<<<< HEAD
 const reviewRouter = express.Router()
 import reviewController from '../controllers/reviews.js'
 // const { ensureAuth } = require(../something)
@@ -9,22 +7,3 @@ reviewRouter.post('/:companyId', reviewController.createReview)
 reviewRouter.put('/:reviewId', reviewController.editReview)
 
 export default reviewRouter
-=======
-import reviewController from '../controllers/reviews.js'
-
-const router = express.Router()
-
-=======
-import reviewController from '../controllers/reviews.js'
-
-const router = express.Router()
-
->>>>>>> 9f55faab1f4ff0c50027537bab4a54d1a6d00dde
-router.post('/:companyId', reviewController.createReview) 
-router.put('/:reviewId', reviewController.editReview)
-
-export default router
-<<<<<<< HEAD
->>>>>>> ac19760 (#2 started edit controller and route)
-=======
->>>>>>> 9f55faab1f4ff0c50027537bab4a54d1a6d00dde
