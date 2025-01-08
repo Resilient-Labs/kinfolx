894fa4734d59ee2c11f65dfacc315744bcd85497
trialnerr <54381853+trialnerr@users.noreply.github.com>
2025-01-08 14:49:15 -0500

    INitial commit for making profile page dynamic. Co-authored-by: Julian Coleman <JColeman1550@users.noreply.github.com>

--- src/components/CompanyReviews/CompanyReviews.jsx
+++ src/components/CompanyReviews/CompanyReviews.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
 import './companyReviews.css'
 
 const CompanyReviews = () => {
+    //do a fetch request and populate the company reviews!
+
+    useEffect( () => {
+        const getUserReviews = async () => {
+            const response = await fetch('/api/review')
+            const userReviews = await response.json()
+            console.log(userReviews, 'userReviews')
+        }
+        getUserReviews()
+        
+    }, [])
     return (
         <div className="review">
             <h3>Reviews You have Left (Only Visible to You)</h3>
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,19 @@
-// import { useEffect } from 'react'
 import './profile.css'
 import ProfileInfo from '../../components/ProfileInfo/ProfileInfo.jsx'
 import CompanyReviews from '../../components/CompanyReviews/CompanyReviews.jsx'
+import { useUser } from '@clerk/clerk-react'
 
 function Profile() {
+    const { isLoaded, user } = useUser()
+    if (!isLoaded || !user) {
+        return
+    }
+    //load a user's reviews
+    //delete button for a review
+    //load the profile pic
+    //load the userName
+    //add an edit button for a review
+
     return (
         <main className="profile-container">
             <ProfileInfo />
