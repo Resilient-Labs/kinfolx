f56a958e76fc5db0143391eed23e321a6136871a
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-01-08 21:13:58 -0500

    Update CompanySalaries.jsx

--- src/components/CompanySalaries/CompanySalaries.jsx
+++ src/components/CompanySalaries/CompanySalaries.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './companySalaries.css'
 
 const CompanySalaries = () => {
@@ -10,16 +9,3 @@ const CompanySalaries = () => {
 }
 
 export default CompanySalaries
-=======
-import './companySalaries.css'
-
-const CompanySalaries = () => {
-    return (
-        <div id="salaries" className="content-box">
-            Summary of Salaries
-        </div>
-    )
-}
-
-export default CompanySalaries
->>>>>>> fa5ac3f743b18acfeda505df07467e6622754c42
