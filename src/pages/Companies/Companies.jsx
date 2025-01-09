230bbe3d4028b40a0276bcdc7a9b10b19e391c1f
Aidan Diaz <diaz.aidan.d@gmail.com>
2025-01-08 21:15:47 -0500

    Update Companies.jsx

--- src/pages/Companies/Companies.jsx
+++ src/pages/Companies/Companies.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './companies.css'
 import CompanyListing from '../../components/CompanyListing/CompanyListing.jsx'
 
@@ -12,18 +11,3 @@ const Companies = () => {
 }
 
 export default Companies
-=======
-import './companies.css'
-import CompanyListing from '../../components/CompanyListing/CompanyListing.jsx'
-
-const Companies = () => {
-    return (
-        <main className="companies-container">
-            <h1 className="page-title">Popular Companies</h1>
-            <CompanyListing />
-        </main>
-    )
-}
-
-export default Companies
->>>>>>> fa5ac3f743b18acfeda505df07467e6622754c42
