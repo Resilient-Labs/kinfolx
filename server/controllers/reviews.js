060f4122b72a33ac493c5546ac5f25295042215e
Erik Jacome <138533562+erikenrique@users.noreply.github.com>
2025-01-09 14:25:54 -0500

    #16 editing createReview to account for the new schema and changes to controller and reviewList.jsx (changes made in call with Abdi), edited getAllCompanyReviews to include companyName and sort

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -57,7 +57,7 @@ const reviewController = {
             }))
 
             console.log(reviewsWithCompanyName)
-            res.json(reviewsWithCompanyName) 
+            res.json(reviewsWithCompanyName.reverse()) 
         } catch (error) {
             next(error)
         }
