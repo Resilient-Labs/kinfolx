6f9dc585359af4f40e4a87e326dd1906ca4404fc
AOA19 <aakinyemi19@gmail.com>
2025-03-29 12:22:12 -0400

    Configured tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -12,5 +13,5 @@ export default defineConfig({
             },
         },
     },
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
 })
