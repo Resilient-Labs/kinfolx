463127be326974bddc26ef14d3e4685de2ea9d9c
Erik Jacome <138533562+erikenrique@users.noreply.github.com>
2025-04-12 16:21:30 -0400

    filter news to just tech and business news #120

--- server/routes/news.js
+++ server/routes/news.js
@@ -6,8 +6,9 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
     try {
-        const response = await fetch(`https://api.thenewsapi.com/v1/news/top?api_token=${process.env.NEWS_API_KEY}&locale=us&limit=5`)
+        const response = await fetch(`https://api.thenewsapi.com/v1/news/top?api_token=${process.env.NEWS_API_KEY}&categories=business,tech&locale=us&limit=5`)
         const json = await response.json()
+        console.log(json)
         res.json(json.data) // return only the array of news articles
     } catch (err) {
         console.error('Failed to fetch news:', err)
